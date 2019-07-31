ceacc33560d5871c2982c38a14685dfd38a73500
kleicht <kleicht@os-os.net>
2019-07-31 11:52:19 +0200

    Implement KlineCandleSticksConverter WriteJson method

--- BinanceExchange.API/Converter/KlineCandleSticksConverter.cs
+++ BinanceExchange.API/Converter/KlineCandleSticksConverter.cs
@@ -12,7 +12,23 @@ namespace BinanceExchange.API.Converter
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            throw new NotImplementedException();
+            var response = value as KlineCandleStickResponse;
+
+            writer.WriteStartArray();
+
+            writer.WriteValue((response.OpenTime - Epoch).TotalMilliseconds);
+            writer.WriteValue(response.Open);
+            writer.WriteValue(response.High);
+            writer.WriteValue(response.Low);
+            writer.WriteValue(response.Close);
+            writer.WriteValue(response.Volume);
+            writer.WriteValue((response.CloseTime - Epoch).TotalMilliseconds);
+            writer.WriteValue(response.QuoteAssetVolume);
+            writer.WriteValue(response.NumberOfTrades);
+            writer.WriteValue(response.TakerBuyBaseAssetVolume);
+            writer.WriteValue(response.TakerBuyQuoteAssetVolume);
+
+            writer.WriteEndArray();
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
@@ -20,17 +36,17 @@ namespace BinanceExchange.API.Converter
             var klineCandlesticks = JArray.Load(reader);
             return new KlineCandleStickResponse
             {
-                OpenTime = Epoch.AddMilliseconds((long) klineCandlesticks.ElementAt(0)),
-                Open = (decimal) klineCandlesticks.ElementAt(1),
-                High = (decimal) klineCandlesticks.ElementAt(2),
-                Low = (decimal) klineCandlesticks.ElementAt(3),
-                Close = (decimal) klineCandlesticks.ElementAt(4),
-                Volume = (decimal) klineCandlesticks.ElementAt(5),
-                CloseTime = Epoch.AddMilliseconds((long) klineCandlesticks.ElementAt(6)),
-                QuoteAssetVolume = (decimal) klineCandlesticks.ElementAt(7),
-                NumberOfTrades = (int) klineCandlesticks.ElementAt(8),
-                TakerBuyBaseAssetVolume = (decimal) klineCandlesticks.ElementAt(9),
-                TakerBuyQuoteAssetVolume = (decimal) klineCandlesticks.ElementAt(10),
+                OpenTime = Epoch.AddMilliseconds((long)klineCandlesticks.ElementAt(0)),
+                Open = (decimal)klineCandlesticks.ElementAt(1),
+                High = (decimal)klineCandlesticks.ElementAt(2),
+                Low = (decimal)klineCandlesticks.ElementAt(3),
+                Close = (decimal)klineCandlesticks.ElementAt(4),
+                Volume = (decimal)klineCandlesticks.ElementAt(5),
+                CloseTime = Epoch.AddMilliseconds((long)klineCandlesticks.ElementAt(6)),
+                QuoteAssetVolume = (decimal)klineCandlesticks.ElementAt(7),
+                NumberOfTrades = (int)klineCandlesticks.ElementAt(8),
+                TakerBuyBaseAssetVolume = (decimal)klineCandlesticks.ElementAt(9),
+                TakerBuyQuoteAssetVolume = (decimal)klineCandlesticks.ElementAt(10),
             };
         }
 
