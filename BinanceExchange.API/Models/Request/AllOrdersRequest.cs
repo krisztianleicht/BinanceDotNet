4eaa1c434db7e7441e3f2c625acd8b4d1beb8dff
kleicht <kleicht@os-os.net>
2019-11-04 22:55:22 +0100

    Add missing fields

--- BinanceExchange.API/Models/Request/AllOrdersRequest.cs
+++ BinanceExchange.API/Models/Request/AllOrdersRequest.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Runtime.Serialization;
+using BinanceExchange.API.Converter;
 using BinanceExchange.API.Models.Request.Interfaces;
+using Newtonsoft.Json;
 
 namespace BinanceExchange.API.Models.Request
 {
@@ -16,6 +19,14 @@ namespace BinanceExchange.API.Models.Request
         public long? OrderId { get; set; }
 
         [DataMember(Order = 3)]
+        [JsonConverter(typeof(EpochTimeConverter))]
+        public DateTime? StartTime { get; set; }
+       
+        [DataMember(Order = 4)]
+        [JsonConverter(typeof(EpochTimeConverter))]
+        public DateTime? EndTime { get; set; }
+
+        [DataMember(Order = 5)]
         public int? Limit { get; set; }
     }
 }
